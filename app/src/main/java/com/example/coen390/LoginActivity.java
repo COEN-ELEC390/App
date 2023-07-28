eb7ba69b7333cd757cd6c8d00e385e4024dcc8e7
ItsDezi <j_desman@live.concordia.ca>
2023-07-28 14:23:15 -0400

    Login page requests notification permissions on log in page

--- app/src/main/java/com/example/coen390/LoginActivity.java
+++ app/src/main/java/com/example/coen390/LoginActivity.java
@@ -3,8 +3,13 @@ package com.example.coen390;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -57,6 +62,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //------------------------------------------notification request
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.POST_NOTIFICATIONS}, 101);
+            }
+        }
+        //------------------------------------------
 
         registerTV = findViewById(R.id.registerLink);
         emailEdit = findViewById(R.id.emailTV);
