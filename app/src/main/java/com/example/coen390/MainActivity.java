37d96415694ddd1340f157fbf86d4b4fae0f155b
Okwilo <veeko9@gmail.com>
2023-08-04 13:53:26 -0400

    Changed Login button colour on activity_login.xml, Added swipe to refresh layout on the main activity_main.xml, changed text in activity_login.xml to "sign up now", Changed text in activity_main.xml to "Dashboard"

--- app/src/main/java/com/example/coen390/MainActivity.java
+++ app/src/main/java/com/example/coen390/MainActivity.java
@@ -1,17 +1,9 @@
 package com.example.coen390;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.FragmentManager;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,6 +15,16 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.coen390.Models.User;
 import com.example.coen390.adapters.EventListAdapter;
@@ -77,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         userAddy = spHelper.getSignedInUserAddress();
         nothingReadyForPickup = findViewById(R.id.noDeliveries);
         eventListView = findViewById(R.id.eventLV);
-        refreshFeed = findViewById(R.id.refreshFeedButton);
+        SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swiperefreshlayout);
         TextView titleText = new TextView(this);
         titleText.setText("Your deliveries");
         titleText.setGravity(25);
@@ -143,14 +145,19 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
         //------------------------------------------------
-        refreshFeed.setOnClickListener(new View.OnClickListener() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onClick(View v) {
+            public void onRefresh() {
                 formattedEventList.clear();
                 unformattedEventList.clear();
                 queryCurrentUserData(getApplicationContext());
-            }
-        });
+        swipeRefreshLayout.setRefreshing(false);
+ if(unformattedEventList.size() == 0)
+            {
+                nothingReadyForPickup.setText("You have no deliveries that are ready for pickup");
+            }};
+
+    });
 //-----------------------------------------document listener
         //if(userAddy != null) {
             final DocumentReference docRef = db.collection("users").document(userAddy);
@@ -218,7 +225,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     User queryCurrentUserData(Context cc)
-    {
+    {Log.d("queryCurrentUserData() being called on the swipe to refresh!", "WOOOOOOOOOOOOHOOOOOOOOO");
         nothingReadyForPickup.setText("");
         final User[] loggedInUser = new User[1];
         db.collection("users")
